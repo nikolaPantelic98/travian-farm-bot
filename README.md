e2f7332bd1f2b54064311714f4e1011683bc5d58
nikola <nikolapantelic998@gmail.com>
2024-02-14 23:36:47 +0100

    Most of the unnecessary logs removed

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Welcome to Travian Farm Bot. This script will fully automate your farming/rading
 
 - Efficiently send all your farm lists 24/7.
 - Storing all logs in the text editor.
-- Telegram API that sends logs to you mobile phone (2 bots).
+- Telegram API that sends logs to you mobile phone.
 
 ## Requirements
 
@@ -51,20 +51,17 @@ sudo pip3 install webdriver-manager
 
 * Install telegram mobile app and make 2 bots:
   - Find BotFather.
-  - write `/newbot`
-  - make 2 bots - one for messages and one for logs
-  - save your token
+  - write `/newbot`.
+  - save your token.
 
 * Set up environment variables on your system:
 ```
-export TRAVIAN_FARM_BOT_USERNAME=]
+export TRAVIAN_FARM_BOT_USERNAME=
 export TRAVIAN_FARM_BOT_PASSWORD=
 export TRAVIAN_FARM_BOT_SERVER=[full https address]
-export TRAVIAN_FARM_BOT_LOG_PATH=[final path for your log folder]
-export TRAVIAN_FARM_BOT_TELEGRAM_MESSAGE_TOKEN=[token that you recieved from telegram]
+export TRAVIAN_FARM_BOT_LOG_PATH=[final path to your log folder]
+export TRAVIAN_FARM_BOT_TELEGRAM_MESSAGE_TOKEN=[token that you recieved from FatherBot]
 export TRAVIAN_FARM_BOT_TELEGRAM_MESSAGE_CHAT_ID=[your telegram chat id]
-export TRAVIAN_FARM_BOT_TELEGRAM_LOG_TOKEN=
-export TRAVIAN_FARM_BOT_TELEGRAM_LOG_CHAT_ID=
 ```
 
 * Start the script:
@@ -77,7 +74,7 @@ python3 script_v1.py
 
 - This script sends attacks approximately every 7 minutes. To change the script execution time, it is necessary to modify the source code. It is recommended that you stick to random numbers.
 - This script only works with Google Chrome browser. In order to change the browser that selenium uses, it is necessary to modify the source code.
-- This script executes only the first farm list in a row. In order for more farm lists to be executed, it is necessary to indicate in the source code 'num_farm_lists=[your_number]` in the line 117.
+- This script executes only the first farm list in a row. In order for more farm lists to be executed, it is necessary to indicate in the source code `num_farm_lists=[your_number]` in the line 117.
 
 
 **Note: The use of this script is against the official rule of the online browser game Travian. The use of this script may cause actions against your account and it is recommended that it be used only for educational purposes. As a developer, I am not responsible for any damage caused by this script.**
--- src/script_v1.py
+++ src/script_v1.py
@@ -40,33 +40,19 @@ def send_telegram_message(message):
     return response.json()
 
 
-def send_telegram_log(message):
-    bot_token = os.environ.get('TRAVIAN_FARM_BOT_TELEGRAM_LOG_TOKEN')
-    chat_id = os.environ.get('TRAVIAN_FARM_BOT_TELEGRAM_LOG_CHAT_ID')
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + message
-
-    response = requests.get(send_text)
-
-    return response.json()
-
-
 def is_connected():
     try:
-        request = requests.get('https://google.com', timeout=1)
-        log("Internet connection is available.")
-        send_telegram_log("Internet connection is available.")
+        requests.get('https://google.com', timeout=1)
         return True
-    except requests.ConnectionError:
-        log("Internet connection is not available.")
+    except requests.ConnectionError as e:
+        log(f"A network error occurred: {e}")
         return False
 
 
 def restart_wifi():
-    log("Restarting wifi...")
     subprocess.run(["nmcli", "radio", "wifi", "off"])
     sleep(2)
     subprocess.run(["nmcli", "radio", "wifi", "on"])
-    log("Wifi restarted. Trying again...")
     sleep(10)
 
 
@@ -75,16 +61,12 @@ def setup_driver():
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-popup-blocking")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    log("Script started!")
-    send_telegram_log("----------------------------------")
-    send_telegram_log("Script started!")
+    log("Starting script...")
     return driver
 
 
 def login(driver, username, password):
     driver.get(os.environ.get('TRAVIAN_FARM_BOT_SERVER'))
-    log("Login form is loaded.")
-    send_telegram_log("Login form is loaded.")
     sleep(random.uniform(2, 4))
 
     username_field = driver.find_element(By.NAME, 'name')
@@ -94,35 +76,22 @@ def login(driver, username, password):
         username_field.send_keys(char)
         sleep(random.uniform(0.12, 0.17))
 
-    log("Username input is filled.")
-    send_telegram_log("Username input is filled.")
-
     for char in password:
         password_field.send_keys(char)
         sleep(random.uniform(0.13, 0.21))
 
-    log("Password input is filled.")
-    send_telegram_log("Password input is filled.")
     sleep(random.uniform(0.1, 0.3))
 
     login_button = driver.find_element(By.XPATH, '//button[contains(@class, "textButtonV1")]')
     login_button.click()
-    log("Login button is clicked.")
-    send_telegram_log("Login button is clicked.")
-    log("------------------------------------------------")
-    send_telegram_log("----------------------------------")
     sleep(random.uniform(7, 10))
 
 
 def send_farm_lists(driver, num_farm_lists=1):
-    log("Loading farm lists.")
-    send_telegram_log("Loading farm lists.")
     driver.get(os.environ.get('TRAVIAN_FARM_BOT_SERVER'))
     sleep(random.uniform(1, 3))
     driver.get(os.environ.get('TRAVIAN_FARM_BOT_SERVER'))
     sleep_time_1 = random.uniform(5, 8)
-    log("Farm lists are loaded.")
-    send_telegram_log("Farm lists are loaded.")
     sleep(sleep_time_1)
 
     buttons = driver.find_elements(By.XPATH, '//button[contains(@class, "textButtonV2")]')
@@ -132,22 +101,14 @@ def send_farm_lists(driver, num_farm_lists=1):
         button = buttons[i]
         button.click()
         log(f"Farm list {i + 1} is sent.")
-        send_telegram_log(f"Farm list {i + 1} is sent.")
         sleep(random.uniform(3, 5))
 
     sleep_time_2 = random.uniform(3.1, 4.1)
-    log("All Farm lists are sent.")
-    send_telegram_log("All farm lists are sent.")
     sleep(sleep_time_2)
-    log("Going to google.")
-    send_telegram_log("Going to google.")
     driver.get('https://google.com')
     sleep_time_3 = random.uniform(402, 416)
     log(f"Script executed! {round(sleep_time_1 + sleep_time_2 + sleep_time_3, 2)} seconds.")
     send_telegram_message(f"Script executed! {round(sleep_time_1 + sleep_time_2 + sleep_time_3, 2)} seconds.")
-    send_telegram_log(f"Script executed! {round(sleep_time_1 + sleep_time_2 + sleep_time_3, 2)} seconds.")
-    log("------------------------------------------------")
-    send_telegram_log(".")
     sleep(sleep_time_3)
 
 
